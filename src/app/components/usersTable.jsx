381e9147b21a1ba1a48b85a5a11681db5616b351
Cojocaru Artiom <artikocon@gmail.com>
2022-08-05 19:08:55 +0300

    Task-5 Completed

---
+++ src/app/components/usersTable.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import TableHeader from './tableHeader';
+import TableBody from './tableBody';
+import BookMark from './bookmark';
+import QualitiesList from './qualitiesList';
+import Table from './table';
+import PropTypes from 'prop-types';
+
+const UsersTable = ({
+   users,
+   onSort,
+   selectedSort,
+   onToggleBookMark,
+   onDelete,
+   ...rest
+}) => {
+   const columns = {
+      name: { path: 'name', name: 'Имя  ' },
+      qualities: {
+         name: 'Качество',
+         component: (user) => <QualitiesList qualities={user.qualities} />
+      },
+      professions: { path: 'profession.name', name: 'Профессия' },
+      completedMettings: { path: 'completedMettings', name: 'Втретился, раз' },
+      rate: { path: 'rate', name: 'Оценка' },
+      bookmark: {
+         path: 'bookmark',
+         name: 'Избранное',
+         component: (user) => (
+            <BookMark
+               status={user.bookmark}
+               onClick={() => onToggleBookMark(user._id)}
+            />
+         )
+      },
+      delete: {
+         component: (user) => (
+            <button
+               onClick={() => onDelete(user._id)}
+               className="btn btn-danger"
+            >
+               delete
+            </button>
+         )
+      }
+   };
+   return (
+      <Table
+         onSort={onSort}
+         selectedSort={selectedSort}
+         columns={columns}
+         data={users}
+      >
+         <TableHeader {...{ onSort, selectedSort, columns }} />
+         <TableBody {...{ columns, data: users }} />
+      </Table>
+   );
+};
+
+UsersTable.propTypes = {
+   users: PropTypes.array.isRequired,
+   onSort: PropTypes.func.isRequired,
+   selectedSort: PropTypes.object.isRequired,
+   onToggleBookMark: PropTypes.func.isRequired,
+   onDelete: PropTypes.func.isRequired
+};
+
+export default UsersTable;
